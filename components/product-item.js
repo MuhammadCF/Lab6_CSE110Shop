88358d2094547ca7fe8fd8816bd27e18671007a3
MuhammadCF <ochafajar@gmail.com>
2021-02-13 23:40:15 -0800

    first submission

--- components/product-item.js
+++ components/product-item.js
@@ -1,7 +1,84 @@
 // product-item.js
-
 class ProductItem extends HTMLElement {
   // TODO
+  constructor(src, alt, ititle, iprice, id) {
+    super();
+    //console.log(id);
+    
+    this.attachShadow({mode:'open'});
+
+    // if(this.hasAttribute('src') && 
+    // this.hasAttribute('alt') &&
+    //  this.hasAttribute('price') && this.hasAttribute('title')){
+
+      
+    //  }
+    // let isource = this.getAttribute('src');
+    // let ialt = this.getAttribute('alt');
+    // let title = this.getAttribute('title');
+    // let price = this.getAttribute('price');
+
+    let isource = src;
+    let ialt = alt;
+    let title = ititle;
+    let price = iprice; 
+
+    const wrapper = document.createElement('li');
+    wrapper.classList.add('product');
+
+    const image = document.createElement('img');
+    image.setAttribute('src', isource);
+    image.setAttribute('alt', ialt);
+
+    const p1 = document.createElement('p');
+    p1.classList.add('title');
+    p1.textContent = title;
+    
+    const p2 = document.createElement('p');
+    p1.classList.add('price');
+    p1.textContent = price;
+
+    const button = document.createElement('button');
+    button.setAttribute('id', id);
+    button.setAttribute('onclick', "alert('Added to Cart!');");
+    button.textContent = 'Add to Cart';
+
+    wrapper.appendChild(image);
+    wrapper.appendChild(p1);
+    wrapper.appendChild(p2);
+    wrapper.appendChild(button);
+
+
+
+    const style = document.createElement('link');
+    style.setAttribute('rel', 'stylesheet');
+    style.setAttribute('href', '../styles/styles.css');
+
+    // const script = document.createElement('script');
+    // script.textContent = `function toggle(id){
+    //   buttonStates[id] = buttonStates[1] ? 0: 1;
+    //   localStorage.setItem('buttonstates', buttonStates);
+    //   const thebutton = document.querySelector('#product-list #' + id);
+    //   if(buttonStates[id]){
+    //     thebutton.textContent = 'Add to Cart';
+    //   }
+    //   else{
+    //     thebutton.textContent = 'Remove from Cart';
+    //   }
+    // }`
+
+    this.shadowRoot.append(style, wrapper);
+    
+
+  }
+
 }
 
 customElements.define('product-item', ProductItem);
+
+// <!-- li class="product">
+// <img src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg" alt="Fjallraven - Foldstack No. 1 Backpack, Fits 15 Laptops" width=200>
+// <p class="title">Fjallraven - Foldstack No. 1 Backpack, Fits 15 Laptops</p>
+// <p class="price">$109.95</p>
+// <button onclick="alert('Added to Cart!')">Add to Cart</button>
+// </li -->
--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,105 @@
 // Script.js
-
+let buttonStates = {};
 window.addEventListener('DOMContentLoaded', () => {
-  // TODO
+
+  //let clothes = "initialize clothes";
+
+
+
+  if(!(clothes = localStorage.getItem('clothes'))){
+
+    
+    fetch('https://fakestoreapi.com/products', {
+      method: 'GET',
+
+    }).then(response => 
+      response.json()).then(data => {
+        clothes = data;
+        localStorage.setItem('clothes', JSON.stringify(data));
+        //console.log(clothes);
+
+        let clothvalues = Object.values(clothes);
+        clothvalues.forEach((cloth) => {
+          buttonStates[cloth.id] = 1;
+        });
+        localStorage.setItem('buttonstates', JSON.stringify(buttonStates));
+        console.log(buttonStates);
+        render(clothvalues);
+
+        //render(clothes);
+        //console.log('this is the if');
+        //console.log(clothes);
+        //console.log(JSON.parse(localStorage.getItem('clothes')));
+      });
+
+  }
+  else{
+    //console.log('this is the else');
+
+    clothes = JSON.parse(localStorage.getItem('clothes'));
+    let clothvalues = Object.values(clothes);
+    clothvalues.forEach((cloth) => {
+      buttonStates[cloth.id] = JSON.parse(localStorage.getItem('buttonstates'))[cloth.id];
+    })
+    // console.log(buttonStates[1]);
+    // console.log(buttonStates[1] ? 0: 1);
+    //console.log(!0);
+    render(clothvalues);
+    //console.log(clothes);
+    //console.log(clothes[0].id);
+    //console.log(Object.values(clothes));
+    //console.log(localStorage.getItem('clothes'));
+
+    
+  }
+
 });
+
+function render(clothes){
+
+  let list = document.getElementById('product-list');
+  clothes.forEach((cloth) => {
+    // let card = document.createElement('product-item');
+    let title = cloth.title;
+    let price = cloth.price;
+    let image = cloth.image;
+    let id = cloth.id;
+    // card.setAttribute('src', image);
+    // card.setAttribute('alt', title);
+    // card.setAttribute('title', title);
+    // card.setAttribute('price', price);
+    let card = new ProductItem(image,title,title,price, id);
+    list.appendChild(card);
+
+
+    // for(id = 1; id <= clothes.length;id++){
+
+    //   const thebutton = document.getElementById(id);
+    //   if(buttonStates[id]){
+    //     thebutton.textContent = 'Add to Cart';
+    //   }
+    //   else{
+    //     thebutton.textContent = 'Remove from Cart';
+    //   }
+    // }
+
+    
+  });
+
+  //this toggle function below can't query the shadow dom, I just realized that
+  //
+
+  function toggle(id){
+      buttonStates[id] = buttonStates[1] ? 0: 1;
+      localStorage.setItem('buttonstates', buttonStates);
+      const thebutton = document.querySelector('#product-list #' + id);
+      if(buttonStates[id]){
+        thebutton.textContent = 'Add to Cart';
+      }
+      else{
+        thebutton.textContent = 'Remove from Cart';
+      }
+  }
+
+
+}
